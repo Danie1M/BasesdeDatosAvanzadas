8139aa0300b90cdf8527dd72b864bc83beaac1c3
Cristian Estrada Peña <cristian357r4@gmail.com>
2018-05-07 10:59:40 -0500

    bootstrap

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 
-	$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=2968239ms") or die('No se ha podido conectar: '.pg_last_error());
+	$dbconn = pg_connect("host=localhost port=5432 dbname=clima user=postgres password=2968239ms") or die('No se ha podido conectar: '.pg_last_error());
+	$result = pg_query($dbconn, "SELECT * FROM usuarios");
+	if (!$result) {
+    	echo "Ocurrió un error.\n";
+   	 exit;
+	}else{
+		echo "la consulta fue realizada con exito\n";
+		echo "<br />\n";
+	}
 
-
+	while ($row = pg_fetch_row($result)) {
+    	echo "\nusuario: $row[1]  \nPassword: $row[2]";
+    	echo "<br />\n";
+	}
 
 ?>
